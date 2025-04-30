f1370862ad32125153f48a7cc0d8e44c4ad00c8a
Alihammoud33 <101220483@mu.edu.lb>
2025-04-30 10:58:31 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: PersonalityTestApp(),
         ),
-      ),
+      
     );
   }
 }
